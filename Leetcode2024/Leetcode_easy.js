e853e504cc1d7e6908cc6e9ecc1bb4b91febe229
Joy Palumbo <greeniejoy@Gmail.com>
2023-12-12 14:12:03 -0500

    Two Sums
    Added Two Sum solution

--- Leetcode2024/Leetcode_easy.js
+++ Leetcode2024/Leetcode_easy.js
@@ -1,1 +1,29 @@
-// setting up git
+// 1. Two Sum 6/29
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // 
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [i, j]
+            }
+        }
+    }
+    };
+
+
+    // 9. Palindrome Number 6/29 RETRY!!!
+
+    /**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let rev = x.toString().split('').reverse().join('');
+      return x.toString() === rev
+  
+  };
